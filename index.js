d833fb606052d3022847c62af66cf3f15462a555
alexkuo40929 <alexkuo40929@gmail.com>
2021-09-23 11:13:09 -0700

    week 4

---
+++ index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
